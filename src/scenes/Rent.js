b5aa6e2b1a0ca8ba51e92a2a1214bcc22c6c7dc9
JunleYuan <leo905991717@gmail.com>
2022-06-01 21:12:08 -0700

    set

--- src/scenes/Rent.js
+++ src/scenes/Rent.js
@@ -7,12 +7,19 @@ class Rent extends Phaser.Scene {
 
     create() {
 
+        storeParts = [];
+        subParts = [];
+
 
         let btext = this.add.bitmapText( 1280/2, 520, 'bm', 'You made $ '+ money , 50).setOrigin(.5, .5).setMaxWidth(400).setAlpha(0);
 
         this.time.delayedCall(1000, () => {
 
-            this.sound.play('star5');
+            if(money > 350){
+
+                this.sound.play('star5');
+            }
+
 
         })
         
@@ -34,48 +41,62 @@ class Rent extends Phaser.Scene {
             duration: 1000
         });
 
-        let star2 = this.add.image(1280/2-50, 380, 'star').setOrigin(.5, .5).setScale(.1).setAlpha(0);
+        if(money > 100){
 
-        this.tweens.add({
-            targets: star2,
-            alpha: 1,
-            delay: 1500,
-            ease: 'Sine.easeInOut',
-            duration: 1000
-        });
+            let star2 = this.add.image(1280/2-50, 380, 'star').setOrigin(.5, .5).setScale(.1).setAlpha(0);
 
-        let star3 = this.add.image(1280/2+50, 380, 'star').setOrigin(.5, .5).setScale(.1).setAlpha(0);
+            this.tweens.add({
+                targets: star2,
+                alpha: 1,
+                delay: 1500,
+                ease: 'Sine.easeInOut',
+                duration: 1000
+            });
 
-        this.tweens.add({
-            targets: star3,
-            alpha: 1,
-            delay: 2000,
-            ease: 'Sine.easeInOut',
-            duration: 1000
-        });
+        }
 
-        let star4 = this.add.image(1280/2-100, 390, 'star').setOrigin(.5, .5).setScale(.1).setAlpha(0);
+        if(money > 200){
 
-        this.tweens.add({
-            targets: star4,
-            alpha: 1,
-            delay: 2500,
-            ease: 'Sine.easeInOut',
-            duration: 1000
-        });
+            let star3 = this.add.image(1280/2+50, 380, 'star').setOrigin(.5, .5).setScale(.1).setAlpha(0);
 
-        let star5 = this.add.image(1280/2+100, 390, 'star').setOrigin(.5, .5).setScale(.1).setAlpha(0);
+            this.tweens.add({
+                targets: star3,
+                alpha: 1,
+                delay: 2000,
+                ease: 'Sine.easeInOut',
+                duration: 1000
+            });
 
-        let star5tw = this.tweens.add({
-            targets: star5,
-            alpha: 1,
-            delay: 3000,
-            ease: 'Sine.easeInOut',
-            duration: 1000,
-            onComplete: function(){ console.log("stars");}
-        });
+        }
 
-        
+        if(money > 300){
+
+            let star4 = this.add.image(1280/2-100, 390, 'star').setOrigin(.5, .5).setScale(.1).setAlpha(0);
+
+            this.tweens.add({
+                targets: star4,
+                alpha: 1,
+                delay: 2500,
+                ease: 'Sine.easeInOut',
+                duration: 1000
+            });
+
+        }
+
+        if(money > 350){
+
+            let star5 = this.add.image(1280/2+100, 390, 'star').setOrigin(.5, .5).setScale(.1).setAlpha(0);
+
+            let star5tw = this.tweens.add({
+                targets: star5,
+                alpha: 1,
+                delay: 3000,
+                ease: 'Sine.easeInOut',
+                duration: 1000,
+                onComplete: function(){ console.log("stars");}
+            });
+
+        }
 
         this.clock = this.time.delayedCall(10000, () => {
 
