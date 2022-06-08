709e6cf07ba2c874ef29bcc2ae115f26e78547a8
Newone88 <81717053+Newone88@users.noreply.github.com>
2022-06-07 22:55:24 -0700

    Pushed Quick Changes

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -7,19 +7,19 @@ class Menu extends Phaser.Scene {
           //Title Screen Animation
           
 
-          this.soundconfig = {
-            mute: false,
-            volume: .1,
-            rate: 1,
-            detune: 0,
-            seek: 0,
-            loop: true,
-            delay: 0
+        this.soundconfig = {
+              mute: false,
+              volume: .1,
+              rate: 1,
+              detune: 0,
+              seek: 0,
+              loop: true,
+              delay: 0
         }
 
         this.sound.play('introMusic', this.soundconfig);
 
-          this.anims.create({ 
+        this.anims.create({ 
             key: 'title', 
             frames: this.anims.generateFrameNames('main_atlas', {      
                 prefix: 'Title-10-FPS--',
@@ -33,35 +33,29 @@ class Menu extends Phaser.Scene {
         });
         
         this.titleScreen = this.add.sprite(0, 0).play('title').setOrigin(0,0).setScale(0.5);
-      
-        
-      
        
         //play button
         this.play = this.add.image(game.config.width/2 - 175, game.config.height/2 + 200, 'play').setOrigin(0.5, 0.5).setScale(0.5).setInteractive();
         this.play.on('pointerdown', () => {
           
-            this.game.sound.stopAll();
-            this.sound.play('buttSound');
+              this.game.sound.stopAll();
+              this.sound.play('buttSound');
 
-            this.scene.start("Day1Scene");
-            /*
-            this.scene.start("UIScene");
-            this.scene.start("buildMain");
-            this.scene.start("inventory");
-            */
+              this.scene.start("Day1Scene");
           });
-          this.play.on('pointerout', () => {
-            this.play.setScale(0.5); 
+
+        this.play.on('pointerout', () => {
+              this.play.setScale(0.5); 
           });
-          this.play.on('pointerover', () => {
-            this.play.setScale(0.6); 
+
+        this.play.on('pointerover', () => {
+              this.play.setScale(0.6); 
           });
 
         //tutorial/controls button
         this.control = this.add.image(game.config.width/2 + 175, game.config.height/2 + 200, 'tutorial').setOrigin(0.5, 0.5).setScale(0.5).setInteractive();
         this.control.on('pointerdown', () => { 
-          this.sound.play('buttSound');
+            this.sound.play('buttSound');
             this.scene.launch('tutorial'); 
         });
         this.control.on('pointerout', () => {
